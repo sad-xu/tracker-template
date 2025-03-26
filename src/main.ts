f40c9889c5863dce3226924c9f8a29bef3e00a66
徐浩聪 <hjv81xo@dingtalk.com>
2025-03-26 18:02:33 +0800

    Add tracker

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,32 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import '@/styles/global.scss';
+import Tracker from '@/utils/tracker.js';
 
 const app = createApp(App);
 
 app.use(router);
+
+// 埋点
+const IS_DEV = process.env.NODE_ENV !== 'production';
+
+const tracker = new Tracker({
+  pageNumLimit: 5,
+  actionNumLimit: 5,
+  showHotSpots: IS_DEV,
+  baseURL: '/api/batch', // () => `${IS_DEV ? '' : '/'}api/point/batch?token=${localStorage.getItem('token')}`
+  directiveType: 'click',
+  logToData: (logList) =>
+    JSON.stringify({
+      burying_id: 1,
+      point_list: logList,
+    }),
+});
+tracker.init({ app, router });
+// Vue.prototype.$tracker = tracker;
+if (IS_DEV) {
+  // @ts-ignore
+  window.tracker = tracker;
+}
 
 app.mount('#app');
